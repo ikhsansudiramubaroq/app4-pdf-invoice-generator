fca82a03a423fb0f1e5f17b13366cb17161e5e98
ikhsan <ikhsansudiramubaroq2910@gmail.com>
2024-10-30 15:26:09 +0700

    menambahkan tanggal

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ for filepath in filepaths:
 
     # Mendapatkan nama file tanpa ekstensi dari path file
     filename = Path(filepath).stem
-
     # Mengambil nomor invoice dari nama file, diasumsikan bahwa formatnya adalah 'nomorInvoice-....xlsx'
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
 
     # Menentukan font dengan jenis "Times", ukuran 16, dan tebal (Bold)
     pdf.set_font(family="Times", size=16, style="B")
-
     # Menambahkan teks ke PDF dengan lebar 50 mm dan tinggi 8 mm, berisi nomor invoice
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+    # Menentukan font dengan jenis "Times", ukuran 16, dan tebal (Bold)
+    pdf.set_font(family="Times", size=16, style="B")
+    # Menambahkan teks ke PDF dengan lebar 50 mm dan tinggi 8 mm, berisi tanggal invoice
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
 
     # Menyimpan file PDF ke dalam folder "PDFs" dengan nama yang sama seperti nama file Excel
     pdf.output(f"PDFs/{filename}.pdf")
