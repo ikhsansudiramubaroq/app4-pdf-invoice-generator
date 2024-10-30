848795b06079e0cb54931c7619ef9d7b0162b0bb
ikhsan <ikhsansudiramubaroq2910@gmail.com>
2024-10-30 15:20:01 +0700

    membuat pdf untuk file excel

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-import  pandas as pd
+# Import library pandas untuk pengolahan data, glob untuk mendapatkan file, FPDF untuk membuat PDF, dan Path dari pathlib untuk manipulasi path file
+import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
+# Mengambil semua file Excel di folder "invoices" dengan ekstensi .xlsx
 filepaths = glob.glob("invoices/*.xlsx")
 
+# Melakukan iterasi pada setiap file Excel di dalam folder "invoices"
 for filepath in filepaths:
-    df = pd.read_excel(filepath,sheet_name="Sheet 1")
-    print(df)
+    # Membaca file Excel dan mengambil data dari sheet "Sheet 1"
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Membuat objek PDF baru dengan orientasi Portrait, satuan ukuran mm, dan format A4
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()  # Menambahkan halaman baru ke PDF
+
+    # Mendapatkan nama file tanpa ekstensi dari path file
+    filename = Path(filepath).stem
+
+    # Mengambil nomor invoice dari nama file, diasumsikan bahwa formatnya adalah 'nomorInvoice-....xlsx'
+    invoice_nr = filename.split("-")[0]
+
+    # Menentukan font dengan jenis "Times", ukuran 16, dan tebal (Bold)
+    pdf.set_font(family="Times", size=16, style="B")
+
+    # Menambahkan teks ke PDF dengan lebar 50 mm dan tinggi 8 mm, berisi nomor invoice
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+
+    # Menyimpan file PDF ke dalam folder "PDFs" dengan nama yang sama seperti nama file Excel
+    pdf.output(f"PDFs/{filename}.pdf")
